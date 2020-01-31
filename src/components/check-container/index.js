74fc2007e55cda1e8658d4ccc436178a9d2a5808
passwd10 <parkinseo94@naver.com>
2020-01-31 22:40:32 +0900

    20200131 TIL

--- src/components/check-container/index.js
+++ src/components/check-container/index.js
@@ -5,7 +5,6 @@ import './index.scss'
 export const CheckConatainer = () => {
   return (
     <div className='check-box'>
-      <p>TIL 총 40 개 작성!</p>
       <p>2020년 TIL 20 개 작성!</p>
     </div>
   );
